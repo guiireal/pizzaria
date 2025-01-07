b993c67b50c2753dbc61b148ff895c764afb6fba
guiireal <guii_real@hotmail.com>
2025-01-07 02:02:00 -0300

    wip

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,7 +1,37 @@
+import { api } from "@/services/api";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 import logo from "../../../public/logo.svg";
 import styles from "../page.module.scss";
+
+async function handleSubmit(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  if (!name || !email || !password) {
+    return;
+  }
+
+  try {
+    const response = await api.post("/users", {
+      name,
+      email,
+      password,
+    });
+
+    if (response.status !== 200) {
+      return;
+    }
+
+    redirect("/");
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 export default function Page() {
   return (
@@ -10,7 +40,7 @@ export default function Page() {
         <Image src={logo} alt="Logo da pizzaria" />
         <section className={styles.login}>
           <h1>Criando sua conta</h1>
-          <form>
+          <form action={handleSubmit}>
             <input
               type="text"
               required
