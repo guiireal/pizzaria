df24bac909a59b4479e0917a4834772b80d78275
guiireal <guii_real@hotmail.com>
2025-01-11 21:13:55 -0300

    wip

--- frontend/src/app/dashboard/components/Orders/index.tsx
+++ frontend/src/app/dashboard/components/Orders/index.tsx
@@ -3,7 +3,9 @@
 import { OrderContext } from "@/providers/order";
 import { Order } from "@/types";
 import { RefreshCw } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { use } from "react";
+import { toast } from "sonner";
 import { Modal } from "../Modal";
 import styles from "./styles.module.scss";
 
@@ -14,16 +16,29 @@ type OrderProps = {
 export function Orders({ orders }: OrderProps) {
   const { isOpen, onRequestOpen } = use(OrderContext);
 
+  const router = useRouter();
+
+  function handleRefresh() {
+    router.refresh();
+    toast.success("Pedidos atualizados com sucesso!");
+  }
+
   return (
     <>
       <main className={styles.container}>
         <section className={styles.header}>
           <h1>Últimos pedidos</h1>
           <button>
-            <RefreshCw size={24} color="#3fffa3" />
+            <RefreshCw size={24} color="#3fffa3" onClick={handleRefresh} />
           </button>
         </section>
         <section className={styles.orders}>
+          {!orders.length && (
+            <span className={styles.emptyItem}>
+              Nenhum pedido aberto no momento...
+            </span>
+          )}
+
           {orders.map((order) => (
             <button
               key={order.id}
