4d1d3b24d7f8a07c2c06282f1ff712bd5d3d0b8d
guiireal <guii_real@hotmail.com>
2025-01-15 01:22:03 -0300

    wip

--- mobile/src/pages/SignIn.tsx
+++ mobile/src/pages/SignIn.tsx
@@ -1,11 +1,34 @@
-import { StyleSheet, Text, View } from "react-native";
-import logo from "../assets/logo.png";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function SignIn() {
   return (
     <View style={styles.container}>
-      <Image source={logo} />
-      <Text>Sign In</Text>
+      <Image style={styles.logo} source={require("../assets/logo.png")} />
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Digite seu e-mail"
+          style={styles.input}
+          placeholderTextColor="#f0f0f0"
+        />
+        <TextInput
+          placeholder="Digite sua senha"
+          style={styles.input}
+          placeholderTextColor="#f0f0f0"
+          secureTextEntry
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -16,5 +39,37 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#1d1d2e",
+  },
+  logo: {
+    marginBottom: 18,
+  },
+  inputContainer: {
+    width: "95%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 34,
+    paddingHorizontal: 14,
+  },
+  input: {
+    width: "95%",
+    height: 40,
+    backgroundColor: "#101026",
+    marginBottom: 12,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    color: "#fff",
+  },
+  button: {
+    width: "95%",
+    height: 40,
+    backgroundColor: "#3fffa3",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+  },
+  buttonText: {
+    color: "#101026",
+    fontWeight: "bold",
+    fontSize: 18,
   },
 });
