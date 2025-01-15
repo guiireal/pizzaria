5f2d37d6beca0d01a8be5949b90674c4f8f3229b
guiireal <guii_real@hotmail.com>
2025-01-14 21:14:12 -0300

    wip

--- mobile/src/pages/SignIn.tsx
+++ mobile/src/pages/SignIn.tsx
@@ -1,9 +1,20 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import logo from "../assets/logo.png";
 
 export default function SignIn() {
   return (
-    <View>
-      <Text>Tela de login</Text>
+    <View style={styles.container}>
+      <Image source={logo} />
+      <Text>Sign In</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1d1d2e",
+  },
+});
